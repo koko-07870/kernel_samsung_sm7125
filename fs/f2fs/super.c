772802c1ce0c9ff59647c91748c25ead62c53f21
Park Ju Hyung <qkrwngud825@gmail.com>
2019-08-09 21:59:59 +0900

    f2fs: improve naming schemes for rapid GC
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1390,7 +1390,7 @@ static void f2fs_put_super(struct super_block *sb)
 	 * above failed with error.
 	 */
 	f2fs_destroy_stats(sbi);
-	f2fs_sbi_list_del(sbi);
+	f2fs_gc_sbi_list_del(sbi);
 
 	/* destroy f2fs internal modules */
 	f2fs_destroy_node_manager(sbi);
@@ -3888,7 +3888,7 @@ try_onemore:
 		goto free_stats;
 	}
 
-	f2fs_sbi_list_add(sbi);
+	f2fs_gc_sbi_list_add(sbi);
 
 	/* read root inode and dentry */
 	root = f2fs_iget(sb, F2FS_ROOT_INO(sbi));
@@ -4046,7 +4046,7 @@ free_node_inode:
 	iput(sbi->node_inode);
 	sbi->node_inode = NULL;
 free_stats:
-	f2fs_sbi_list_del(sbi);
+	f2fs_gc_sbi_list_del(sbi);
 	f2fs_destroy_stats(sbi);
 free_nm:
 	f2fs_destroy_node_manager(sbi);
